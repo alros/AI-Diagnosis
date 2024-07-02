0f0dbfb0c3535db5761cadf787865bd3e0ea2e11
Alberto Rossotto <alberto.rossotto@gmail.com>
2024-07-02 21:26:50 +0200

    add tests

--- tester/test.py
+++ tester/test.py
@@ -1,3 +1,7 @@
+import json
+import sys
+from json import JSONDecodeError
+
 from llama_index.llms.ollama import Ollama
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.response_synthesizers import ResponseMode
@@ -6,34 +10,57 @@ from llama_index.core import ChatPromptTemplate
 from llama_index.core import Settings
 from chatbot.txt_retriever import TxtRetriever
 from chatbot.config import Config
+from test_cases import tests
 
-model = 'mistral'
 collection = 'Dementia'
-
-Settings.llm = Ollama(model=model)
-
-vector_retriever_chunk = TxtRetriever(collection)
-
-text_template = ChatPromptTemplate([
-            ChatMessage(
-                role=MessageRole.SYSTEM,
-                content='\n'.join(Config.get('prompts.diagnosis.system')),
-            ),
-            ChatMessage(
-                role=MessageRole.USER,
-                content='\n'.join(Config.get('prompts.diagnosis.user')),
-            ),
-        ])
+prompt = '1'
 
 
-query_engine = RetrieverQueryEngine.from_args(
-                vector_retriever_chunk,
-                verbose=True,
-                response_mode=ResponseMode.COMPACT,
-                text_qa_template=text_template)
+def test(model: str, temperature: float):
+    Settings.llm = Ollama(model=model, temperature=temperature)
 
-prompt = 'The 75-year-old patient is experiencing age-related cognitive decline, particularly in recognizing people and following complex conversations, as well as occasional memory lapses and disorientation.'
+    vector_retriever_chunk = TxtRetriever(collection)
 
-response = query_engine.query(prompt)
+    text_template = ChatPromptTemplate([
+        ChatMessage(
+            role=MessageRole.SYSTEM,
+            content='\n'.join(Config.get('prompts.diagnosis.system')),
+        ),
+        ChatMessage(
+            role=MessageRole.USER,
+            content='\n'.join(Config.get('prompts.diagnosis.user')),
+        ),
+    ])
 
-print(response.response)
+    query_engine = RetrieverQueryEngine.from_args(
+        vector_retriever_chunk,
+        verbose=True,
+        response_mode=ResponseMode.COMPACT,
+        text_qa_template=text_template)
+
+    for test in tests:
+        response = query_engine.query(test['prompt'])
+        diagnosis = response.response
+        if not diagnosis.endswith('}'):
+            diagnosis = diagnosis + '}'
+        try:
+            extract = json.loads(diagnosis)
+            print(';'.join([model, prompt, str(temperature), test['id'],
+                            str(extract['Number']),
+                            test['expected'], test['prompt'],
+                            extract['Explanation']]))
+        except JSONDecodeError:
+            print('error parsing json', file=sys.stderr)
+            print(response.response, file=sys.stderr)
+
+
+print(';'.join(['model', 'prompt', 'temperature', 'test id', 'symptoms detected',
+                'symptoms expected', 'input',
+                'explanation']))
+
+for cur_model in [
+        #'mistral',
+        'llama2'
+    ]:
+    for cur_temperature in [0.75, 0.60, 0.45, 0.30, 0.15, 0]:
+        test(cur_model, cur_temperature)
---
+++ tester/test_cases.py
@@ -0,0 +1,37 @@
+tests = [
+    {
+        'id': '1',
+        'expected': '0',
+        'prompt': 'The patient is a 75-year-old individual.'
+    },
+    {
+        'id': '2',
+        'expected': '1',
+        'prompt': 'The patient is a 75-year-old individual experiencing memory difficulties, specifically with recognizing people he has met before.'
+    },
+    {
+        'id': '3',
+        'expected': '1',
+        'prompt': 'The patient is a 75-year-old individual who is exhibiting signs of memory loss, specifically forgetting recently encountered people, but reports no difficulties with reading or writing.'
+    },
+    {
+        'id': '4',
+        'expected': '2',
+        'prompt': 'The 75-year-old patient is experiencing age-related cognitive decline, specifically in recognizing people and following complex verbal information.'
+    },
+    {
+        'id': '5',
+        'expected': '2',
+        'prompt': 'The 75-year-old patient is experiencing age-related cognitive decline, evidenced by difficulty recognizing people and following conversations, but denies issues with reading, writing, or mobility.'
+    },
+    {
+        'id': '6',
+        'expected': '3',
+        'prompt': 'The 75-year-old patient is experiencing age-related cognitive decline, as evidenced by difficulty recognizing people, following conversations, and navigating unfamiliar environments.'
+    },
+    {
+        'id': '7',
+        'expected': '4',
+        'prompt': 'The 75-year-old patient is experiencing age-related cognitive decline, particularly in recognizing people and following complex conversations, as well as occasional memory lapses and disorientation.'
+    },
+]
